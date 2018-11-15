04a129dc5e666ec4f03b4c80afcf26aacdf7fa23
magpia13 <mag.pia13@gmail.com>
2018-11-15 17:27:33 +0100

    sda

--- client/build/precache-manifest.0bcbf2965bf588de498d60dea280be58.js
+++ client/build/precache-manifest.0bcbf2965bf588de498d60dea280be58.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "3250933e94320066f0bf",
-    "url": "./static/js/main.3250933e.chunk.js"
+    "revision": "d0aafe26662da8831897",
+    "url": "./static/js/main.d0aafe26.chunk.js"
   },
   {
     "revision": "d6eabd7ee8ebb82e0f12",
     "url": "./static/js/1.d6eabd7e.chunk.js"
   },
   {
-    "revision": "3250933e94320066f0bf",
+    "revision": "d0aafe26662da8831897",
     "url": "./static/css/main.3aa2b6be.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "./static/css/1.cad1bf54.chunk.css"
   },
   {
-    "revision": "72a4e391b9d5a599aa93ee298e9d3717",
+    "revision": "6d5cb9bf50000965999c64c5eba1cfe2",
     "url": "./index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.443fb671b2e554a5e2b2a8eb671bf996.js"
+  "./precache-manifest.0bcbf2965bf588de498d60dea280be58.js"
 );
 
 workbox.clientsClaim();
