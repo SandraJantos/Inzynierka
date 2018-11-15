f1b9f54bf5acbb19a32652fea9e5cf164bdba853
magpia13 <mag.pia13@gmail.com>
2018-11-15 02:43:41 +0100

    fd

---
+++ client/build/precache-manifest.aed849596feaca33b81bc942cc6e9e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0269343384d80dd1a4d1",
+    "url": "/static/js/main.02693433.chunk.js"
+  },
+  {
+    "revision": "6acb232989db20ddd463",
+    "url": "/static/js/1.6acb2329.chunk.js"
+  },
+  {
+    "revision": "0269343384d80dd1a4d1",
+    "url": "/static/css/main.134e616f.chunk.css"
+  },
+  {
+    "revision": "6acb232989db20ddd463",
+    "url": "/static/css/1.c6b7b97e.chunk.css"
+  },
+  {
+    "revision": "f48d2156c4f438d81e00bff310f875c2",
+    "url": "/index.html"
+  }
+];
